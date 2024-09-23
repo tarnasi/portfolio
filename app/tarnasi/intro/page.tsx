33f066fd0ea7b0274e7c00ced2d4fcff5c349f2f
Shahriyar <shahryar.tarnasi@gmail.com>
2024-09-23 16:59:55 +0400

    update my site

---
+++ app/tarnasi/intro/page.tsx
@@ -0,0 +1,8 @@
+export default function Portfolio() {
+    return (
+        <div>
+            <p>This page is under construction wait a little</p>
+            <p>In the mean time you can contact me with this email: shahryar.tarnasi@gmail.com</p>
+        </div>
+    )
+}
