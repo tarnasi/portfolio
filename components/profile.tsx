f4414bbbdd7e151367b3fda4d75f3f3976f610aa
Shahriyar <shahryar.tarnasi@gmail.com>
2024-09-24 16:36:41 +0400

    one step forward

--- components/profile.tsx
+++ components/profile.tsx
@@ -1,45 +1,41 @@
+/* eslint-disable */
 import { CodeOutlined, GithubOutlined, ProjectOutlined } from '@ant-design/icons';
 import Link from 'next/link';
 
-export default function Profile() {
+export default async function Profile() {
     return (
         <div id="profile" className="text-txt text-justify bg-mainColor w-full h-1/2 m-10 rounded shadow-md p-6 shadow-secondColor">
-            <h2 className="text-7xl">{`Shahriyar Tarnasi`}</h2>
-            <p className='mt-10'>
-                {`I am a <b>Senior Full Stack Programmer</b> with over 6 years of experience crafting scalable, high-performance web applications. Passionate about coding, I thrive on solving complex problems and continuously learning new technologies.`}
-            </p>
-            <p className='mt-2'>
-                {`When I amm not coding, you will find me immersed in <b>Anime</b>, <b>Gaming</b>, or <b>exploring the world</b> through <b>Travel</b>. I'm driven by creativity and the desire to build systems that make a real impact.`}
-            </p>
-            <p className='mt-2'>
-                {`I am a Developer, Gamer, and an Anime lover :)`}
-            </p>
+            <h2 className="text-7xl">Shahriyar Tarnasi</h2>
+            <p className='mt-10'>I'm a <b>Senior Full Stack Programmer</b> with over 6 years of experience crafting scalable, high-performance web applications. Passionate about coding, I thrive on solving complex problems and continuously learning new technologies.</p>
+            <p className='mt-2'>When I'm not coding, you’ll find me immersed in <b>Anime</b>, <b>Gaming</b>, or <b>exploring the world</b> through <b>Travel</b>. I'm driven by creativity and the desire to build systems that make a real impact.</p>
+            <p className='mt-2'>I'm a Developer, Gamer and a anime lover :)</p>
 
-            <div className="flex justify-around mt-10 flex-wrap">
+            <div className="flex justify-around mt-10">
                 <Link href="/projects" className="w-full sm:w-1/2 lg:w-1/4">
                     <div className="p-6 bg-secondColor shadow-lg hover:shadow-xl rounded-lg transition-shadow duration-300 ease-in-out text-center">
                         <CodeOutlined className="text-4xl mb-4 text-mainColor" />
-                        <h3 className="text-xl font-semibold">{`Projects`}</h3>
-                        <p>{`View my latest coding projects`}</p>
+                        <h3 className="text-xl font-semibold">Projects</h3>
+                        <p>View my latest coding projects</p>
                     </div>
                 </Link>
 
                 <Link href="https://github.com/tarnasi" className="w-full sm:w-1/2 lg:w-1/4">
                     <div className="p-6 bg-secondColor shadow-lg hover:shadow-xl rounded-lg transition-shadow duration-300 ease-in-out text-center">
                         <GithubOutlined className="text-4xl mb-4 text-fifthColor" />
-                        <h3 className="text-xl font-semibold">{`GitHub`}</h3>
-                        <p>{`Check out my repositories`}</p>
+                        <h3 className="text-xl font-semibold">GitHub</h3>
+                        <p>Check out my repositories</p>
                     </div>
                 </Link>
 
                 <Link href="/experience" className="w-full sm:w-1/2 lg:w-1/4">
                     <div className="p-6 bg-secondColor shadow-lg hover:shadow-xl rounded-lg transition-shadow duration-300 ease-in-out text-center">
                         <ProjectOutlined className="text-4xl mb-4 text-thirdColor" />
-                        <h3 className="text-xl font-semibold">{`Experience`}</h3>
-                        <p>{`Learn about my work experience`}</p>
+                        <h3 className="text-xl font-semibold">Experience</h3>
+                        <p>Learn about my work experience</p>
                     </div>
                 </Link>
             </div>
+
         </div>
-    );
+    )
 }
